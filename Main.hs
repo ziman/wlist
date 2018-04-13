63c0f818d5595f463c449944293fa5d96e8f86b6
Matus Tejiscak <ziman@functor.sk>
2018-04-13 10:32:03 +0200

    Add AP selection.

--- Main.hs
+++ Main.hs
@@ -27,9 +27,10 @@ optParserInfo = Opts.info (optParser <**> Opts.helper)
     <> Opts.progDesc "Select WiFi network on IFACE"
     <> Opts.header "wlist - WiFi network selector")
 
-type Essid ap = (String, [ap])
+type SSID = String
+type Essid ap = (SSID, [ap])
 
-groupEssids :: [AP] -> M.Map String [AP]
+groupEssids :: [AP] -> M.Map SSID [AP]
 groupEssids = foldr (\ap -> M.insertWith (++) (apEssid ap) [ap]) M.empty
 
 arrange :: [Essid AP] -> [Essid (Int, AP)]
@@ -40,6 +41,9 @@ arrange = go 1
         (essid, zip [n..] . reverse $ sortBy (comparing apSignal) aps)
         : go (n + length aps) rest
 
+flatten :: [Essid (Int, AP)] -> [(Int, SSID, AP)]
+flatten = concatMap (\(ssid, aps) -> [(i,ssid,ap) | (i,ap) <- aps])
+
 fmtItem :: Essid (Int, AP) -> [String]
 fmtItem (essid, aps) = essid : map ("  " ++) subs
   where
@@ -48,14 +52,30 @@ fmtItem (essid, aps) = essid : map ("  " ++) subs
         | (i, AP{..}) <- aps
         ]
 
+connectTo :: Options -> (SSID, AP) -> IO ()
+connectTo Options{..} (ssid, AP{..}) = do
+    callProcess "dhcpcd" ["-k", optIface]
+    callProcess "iw" [optIface, "connect", ssid, apBssid]
+    callProcess "dhcpcd" [optIface]
+
 main :: IO ()
 main = do
     opts@Options{..} <- Opts.execParser optParserInfo
     callProcess "ip" ["link", "set", optIface, "up"]
-    essids <- groupEssids . parse <$> readProcess "iw" [optIface, "scan"] ""
-    putStr . unlines . concatMap fmtItem . arrange $ M.toList essids
+    essids <- arrange . M.toList . groupEssids . parse 
+        <$> readProcess "iw" [optIface, "scan"] ""
+    putStr . unlines . concatMap fmtItem $ essids
 
-    putStr "pick your wifi > "
+    putStr "pick your wifi (or ^C to cancel)> "
     hFlush stdout
-    resp <- getLine
-    return ()
+
+    let chooseAP = do
+            resp <- read <$> getLine
+            case filter (\(i,_,_) -> i == resp) (flatten essids) of
+                [] -> do
+                    putStrLn $ "no such AP: " ++ show resp
+                    chooseAP
+                [(i,essid,ap)] -> return (essid, ap)
+                aps  -> error $ "should never happen: multiple APs with same id: " ++ show aps
+
+    connectTo opts =<< chooseAP
